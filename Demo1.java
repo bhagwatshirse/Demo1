51bf3efe5cb455f4051f81ef82e46ade89440844
bhagwat <bshirse1@gmail.com>
2023-01-19 19:30:46 +0530

    new demo1 added

---
+++ Demo1.java
@@ -0,0 +1,18 @@
+package com.velocity;
+
+public class Demo1 {
+	static int a = 10;
+	int b=20;
+
+	public static void main(String[] args) {
+		Demo1 d=new Demo1();
+		Demo1 d1=new Demo1();
+		Demo1 d2=new Demo1();
+		System.out.println(d.a++ );
+		System.out.println(d1.a++ );
+		System.out.println(d2.a++ );
+		
+
+	}
+
+}
